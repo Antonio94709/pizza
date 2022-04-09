d3e8313ec4c56c1a600933fa4ed93fc9b3eb9208
Antonio94709 <exilhekkiekassongo6@gmail.com>
2022-04-09 11:44:30 +0200

    sixth commit

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -5,8 +5,33 @@ import axios from 'axios'
 
 
 const Product = ({ pizza }) => {
+  console.log(pizza);
+  const [quantity, setQuantity] = useState(1)
+  const [extra, setExtra] = useState([])
   const [size, setSize] = useState(0)
- 
+  const [price, setPrize] = useState(pizza.prices[0])
+
+  const changePrice = (number) => {
+    setPrize(price + number)
+  }
+  const handleSize = (sizeIndex) => {
+    const difference = pizza.prices[sizeIndex] - pizza.prices[size]
+    setSize(sizeIndex)
+    changePrice(difference)
+  }
+  const handleChange = (e, option) => {
+    const checked = e.target.checked
+    if (checked) {
+      changePrice(option.price)
+      setExtra((prev) => [...prev,option])
+    } else {
+      changePrice(-option.price)
+      setExtra(extra.filter((extra) => extra._id !== option._id))
+    }
+  }
+
+  console.log(extra);
+
   return (
     <div className="flex" style={{ height: '70vh' }}>
       <div className="relative h-full flex-1 items-center justify-center">
@@ -17,26 +42,26 @@ const Product = ({ pizza }) => {
       <div className="m-auto ml-36 flex-1 ">
         <p className="pb-3 text-3xl font-bold">{pizza.name}</p>
         <span className="pb-10 text-lg font-semibold text-red-600 underline">
-          ${pizza.prices[size]}
+          ${price}
         </span>
         <p className="font-semibold text-gray-700">{pizza.desc}</p>
         <p className="pt-5 text-lg font-bold">Choose size</p>
         <div className="flex">
-          <div onClick={() => setSize(0)}>
-            <Image src="/Images/zasvg.png" width="50" height="50"/>
+          <div onClick={() => handleSize(0)}>
+            <Image src="/Images/zasvg.png" width="50" height="50" />
             {/* <PhoneIcon className="mx-8 h-6 w-6 cursor-pointer" /> */}
             <span className="top-0 right-0 rounded-lg bg-black p-1 text-xs text-white">
               Small
             </span>
           </div>
-          <div onClick={() => setSize(1)}>
-          <Image src="/Images/zasvg.png" width="70" height="70"/>
+          <div onClick={() => handleSize(1)}>
+            <Image src="/Images/zasvg.png" width="70" height="70" />
             <span className="rounded-lg bg-black p-1 text-xs text-white">
               Medium
             </span>
           </div>
-          <div onClick={() => setSize(2)}>
-          <Image src="/Images/zasvg.png" width="90" height="90"/>
+          <div onClick={() => handleSize(2)}>
+            <Image src="/Images/zasvg.png" width="90" height="90" />
             <span className="rounded-lg bg-black p-1 text-xs text-white">
               Large
             </span>
@@ -45,26 +70,16 @@ const Product = ({ pizza }) => {
         <div className="pt-7">
           <p>Choose additonal topping</p>
           <div className="p-3 flex">
-            <div className="mx-1">
-              <input type="checkbox" id="double" name="double"  />
-              <label htmlFor="double" className= "text-sm font-bold ml-1">Double ingredients</label>
-            </div>
-            <div className="mx-1">
-              <input type="checkbox" id="cheese" name="cheese" className='' />
-              <label htmlFor="cheese" className= "text-sm font-bold ml-1">Cheese sauce</label>
-            </div>
-            <div className="mx-1">
-              <input type="checkbox" id="spicy" name="spicy"  />
-              <label htmlFor="spicy" className= "text-sm font-bold ml-1">Spicy sauce</label>
-            </div>
-            <div className="mx-1">
-              <input type="checkbox" id="garlic" name="garlic" />
-              <label htmlFor="garlic" className= "text-sm font-bold ml-1">Garlic sauce</label>
-            </div>
+            {pizza.extraOption.map((option) => (
+              <div className="mx-1" key={option._id}>
+                <input type="checkbox" id={option.text} name={option.text} onChange={(e) => handleChange(e, option)} />
+                <label htmlFor="double" className="text-sm font-bold ml-1">{option.text}</label>
+              </div>
+            ))}
           </div>
           <div className='flex mt-5 '>
-              <input type='number' defaultValue={1} className=" bg-gray-300 rounded w-10 h-8"/>
-              <button className='h-8 text-center text-white ml-2 bg-red-600 p-1 rounded'>Add to cart</button>
+            <input type='number' defaultValue={1} className=" bg-gray-300 rounded w-10 h-8" onChange={(e) => setQuantity(e.target.value)} />
+            <button className='h-8 text-center text-white ml-2 bg-red-600 p-1 rounded'>Add to cart</button>
           </div>
         </div>
       </div>
@@ -72,7 +87,7 @@ const Product = ({ pizza }) => {
   )
 }
 
-export const getServerSideProps = async ({params}) => {
+export const getServerSideProps = async ({ params }) => {
   const res = await axios.get(`http://localhost:3000/api/products/${params.id}`)
 
   return {
